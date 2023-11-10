1a43d9c2a115d2011a185f1930eb9ba735c3d187
OstrichWhite <rahimcsestudies@gmail.com>
2023-11-11 02:23:28 +0530

    Setup mongoose mongodb connection.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connected successfully"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
