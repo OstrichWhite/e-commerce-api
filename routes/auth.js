264dda1d2cd49263cefccb6cfb9af85673046ada
OstrichWhite <rahimcsestudies@gmail.com>
2023-11-11 13:43:20 +0530

    Introduced accessToken.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   const newUser = new User({
@@ -31,9 +32,13 @@ router.post("/login", async (req, res) => {
     );
     const password = hashedPassword.toString(CryptoJS.enc.Utf8);
     password !== req.body.password && res.status(401).json("wrong credentials");
-
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
     const { password: dummy, ...rest } = user._doc;
-    res.status(200).json(rest);
+    res.status(200).json({ ...rest, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
